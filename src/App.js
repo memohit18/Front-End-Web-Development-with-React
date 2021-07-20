8872daea4150257f86f91e3c4aa0442b099ba16b
memohit18 <mohit2010sm@gmail.com>
2021-07-20 15:54:58 +0530

    component-render

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div >
-      {/* Start of Navbar Component */}
-      <Navbar dark color = "primary">
-        <div className = "container">
-          <NavbarBrand href="/">Ristrorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      {/* End of Navbar Component */}
+class App extends Component{
 
-      {/* Start of Menu Component */}
-      <Menu />
-      {/* End of Menu Component */}
+  constructor(props){
+    super(props);
 
-    </div>
-  );
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div >
+        {/* Start of Navbar Component */}
+        <Navbar dark color = "primary">
+          <div className = "container">
+            <NavbarBrand href="/">Ristrorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        {/* End of Navbar Component */}
+  
+        {/* Start of Menu Component */}
+        <Menu dishes = {this.state.dishes} />
+        {/* End of Menu Component */}
+  
+      </div>
+    );
+  }
 }
 
 export default App;
